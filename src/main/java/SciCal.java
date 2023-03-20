48cf2fde549f062a9f0b82191b5142a31aa3cacc
Abhijith18 <saiabhi.ind@gmail.com>
2023-03-21 02:09:14 +0530

    log4j2

--- src/main/java/SciCal.java
+++ src/main/java/SciCal.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SciCal {
+    private static final Logger logger = LogManager.getLogger(SciCal.class);
     public static void main(String[] args)
     {
         SciCal cal = new SciCal();
@@ -17,6 +21,8 @@ public class SciCal {
         int fact = 1;
         for(int i=1; i<n+1; i++)
             fact *= i;
+        logger.info("[FACTORIAL] - " + n);
+        logger.info("[RESULT - FACTORIAL] - " + fact);
         return fact;
     }
 }
